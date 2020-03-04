feca8ddd0bca1898b2d2acbf4916f9e26fbd5dbe
Junxi Li <jimkeecn@gmail.com>
2020-03-04 17:50:00 +1100

    complete web component

--- projects/external/src/app/app.module.ts
+++ projects/external/src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Injector } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { createCustomElement } from "@angular/elements";
+import { SharedModule } from "projects/library/src/shared/shared.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  //bootstrap: [AppComponent],
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const AppWebComponent = createCustomElement(AppComponent, { injector });
+    customElements.define("subject-table-view", AppWebComponent);
+    /**can add more web component here......
+     *
+     * exmaple:
+     * const secondComponent = create.......(){}
+     * customElements.define("second-component",secondComponent)
+     */
+  }
+  ngDoBootstrap() {}
+}
