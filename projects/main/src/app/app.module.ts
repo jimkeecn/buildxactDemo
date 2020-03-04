326faa6e9f826a4bee564685c749abfc0a2bbe56
Junxi Li <jimkeecn@gmail.com>
2020-03-04 15:21:34 +1100

    add navbar, add plans module, add docments module

--- projects/main/src/app/app.module.ts
+++ projects/main/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SharedModule } from "projects/library/src/shared/shared.module";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ projects/main/src/modules/documents/documents.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DocumentsComponent } from './documents/documents.component';
+
+
+
+@NgModule({
+  declarations: [DocumentsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DocumentsModule { }
